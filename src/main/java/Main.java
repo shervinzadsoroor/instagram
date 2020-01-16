71aadb6d2e3d395868757ce67e7d22fce250d960
shervin zadsoroor <shervinzadsoroor@gmail.com>
2020-01-16 16:52:07 +0330

    change pass method implemented

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,6 +7,7 @@ public class Main {
     public static void main(String[] args) {
 
         Account account = null;
+        AccountCrud accountCrud = new AccountCrud();
         Scanner scanner = new Scanner(System.in);
         String command = null;
 
@@ -17,18 +18,28 @@ public class Main {
             if (account == null) {
                 System.out.println("what do you want? ( sign up | sign in ):");
                 command = scanner.nextLine();
-                AccountCrud accountCrud = new AccountCrud();
+
                 if (command.equalsIgnoreCase("sign up")) {
                     accountCrud.signUp();
                 } else if (command.equalsIgnoreCase("sign in")) {
-                    accountCrud.signIn();
+                    account = accountCrud.signIn();
                 } else {
                     System.out.println("wrong command!!!");
                 }
             }
 
             if (account != null) {
-
+                System.out.println("what do you want? ( change pass | delete account | log out ):");
+                command = scanner.nextLine();
+                if (command.equalsIgnoreCase("change pass")) {
+                    accountCrud.changePass(account);
+                } else if (command.equalsIgnoreCase("delete account")) {
+                    accountCrud.delete();
+                } else if (command.equalsIgnoreCase("log out")) {
+                    account = null;
+                } else {
+                    System.out.println("wrong command !!!");
+                }
             }
         }
     }
--- src/main/java/crudrepositories/AccountCrud.java
+++ src/main/java/crudrepositories/AccountCrud.java
@@ -5,6 +5,8 @@ import models.Account;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 
+import javax.persistence.Query;
+import java.util.List;
 import java.util.Scanner;
 
 public class AccountCrud {
@@ -36,10 +38,60 @@ public class AccountCrud {
         Session session = sessionFactory.openSession();
         session.beginTransaction();
 
+        Account account = null;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("enter username: ");
+        String username = scanner.nextLine();
 
+        System.out.println("enter password: ");
+        String password = scanner.nextLine();
+
+        List<Account> list = session.createQuery("from Account where username= :username")
+                .setParameter("username", username)
+                .list();
+
+        if (list.size() > 0) {
+            account = list.get(0);
+            System.out.println("sign in successful !!!");
+        } else {
+            System.out.println("account does not exist !!! ");
+        }
 
         session.getTransaction().commit();
         session.close();
-        return null;
+        return account;
     }
+
+    public void changePass(Account account) {
+        SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
+        Session session = sessionFactory.openSession();
+        session.beginTransaction();
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("enter new password");
+        String firstNewPass = scanner.nextLine();
+
+        System.out.println("enter new password");
+        String secondNewPass = scanner.nextLine();
+
+        if (firstNewPass.equals(secondNewPass)) {
+            Query query = session.createQuery("update Account set password=:password where username=:username")
+                    .setParameter("password", firstNewPass)
+                    .setParameter("username", account.getUsername());
+            query.executeUpdate();
+        }
+
+        session.getTransaction().commit();
+        session.close();
+    }
+
+    public void delete() {
+        SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
+        Session session = sessionFactory.openSession();
+        session.beginTransaction();
+
+        session.getTransaction().commit();
+        session.close();
+    }
+
 }
